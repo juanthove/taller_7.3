dfec2ddd4af5c041849ae28532ef39930a542a1f
nogue06 <agusnogue@gmail.com>
2025-08-13 14:09:06 -0300

    Modifico validar  mínimo contraseña
    Cambie la función que hice antes para trabajar con la variable error

--- js/validacion.js
+++ js/validacion.js
@@ -7,15 +7,29 @@ function showAlertError() {
 }
 
 document.getElementById("regBtn").addEventListener("click", function(){
-    const password1 = document.getElementById("password1").value;
+    let error = false;
+    let campos = document.getElementsByClassName("form-control");
+    for(let i = 0; i<campos.length; i++){
+        if(campos[i].value.trim()==""){
+            error = true;
+        }
+    }
 
-    if (validarPassword(password1)) {
-        showAlertSuccess();
-    } else {
+     let terminos = document.getElementById("terminos")
+
+    if (!terminos.checked) {
+        error = true;
+    }
+
+    //Validacion minimo de caracteres en la contraseña
+    let password1 = document.getElementById("password1").value;
+    if (password1.length < 6) {
+        error = true;
+    }
+
+    if(error){
         showAlertError();
+    }else{
+        showAlertSuccess();
     }
 });
-
-function validarPassword(password){
-    return password.length >=6;
-}
